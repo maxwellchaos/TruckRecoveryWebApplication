bd986c85558d05a0b668e13ec66536eaabfc77a6
AlexInyaev <46852628+AlexInyaev@users.noreply.github.com>
2022-05-06 02:40:20 +0300

    добавил код js который выводит в консоль название кнопки и текст из textarea

--- TruckRecoveryWebApplication/wwwroot/js/site.js
+++ TruckRecoveryWebApplication/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+let butoon = document.querySelector('.add-work');
+butoon.onclick = () => {
+   
+    let arr = new Array();
+    arr.push(document.querySelector('.work-report').value);
+    arr.push(' Название кнопки : Добавить работу по ремонту');
+    let obj = {};
+    obj['отчет по диагностике'] = document.querySelector('.work-report').value;
+    obj['Название кнопки'] = 'Добавить работу по ремонту';
+
+    console.log(`${document.querySelector('.work-report').value} Название кнопки : Добавить работу по ремонту`);
+    console.log(arr);
+    console.log(obj);
+
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://localhost:7270/Repairs/Create?OrderId=4");
+    xhr.send();
+}
